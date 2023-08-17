fc367823a30e17b0dca9da087a35cb3e5c0f941b
ZiarayZ <jakesmith@binaryrage.com>
2023-08-17 20:43:34 +0100

    movement stats

--- src/client/src/entity/entity.py
+++ src/client/src/entity/entity.py
@@ -3,7 +3,14 @@ import pygame
 
 
 class Entity(pygame.sprite.Sprite):
+    force = 0.5
+    friction = -0.12
+
     def __init__(self, width: int = 50, height: int = 50):
         super().__init__()
         self.surf = pygame.Surface((width, height))
         self.rect = self.surf.get_rect()
+
+        self.pos = pygame.math.Vector2((0, 0))
+        self.vel = pygame.math.Vector2(0, 0)
+        self.acc = pygame.math.Vector2(0, 0)
